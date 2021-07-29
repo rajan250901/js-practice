6ff166041513cd0792b66c1899158a951d7c18c9
rajan250901 <rajan250901@gmail.com>
2021-07-29 09:45:01 +0530

    all first letter in upper case

--- synchronous.js
+++ synchronous.js
@@ -9,26 +9,26 @@ const data = {
   };
 function breakfast()
 {
-    console.log("eat " + data.food1[0] + " and " +data.food1[4] );
+    console.log("Eat " + data.food1[0] + " and " +data.food1[4] );
 }
 function process(argument)
 {
-    console.log("wake up at 7 am");
-    console.log("brush your teeth");
+    console.log("Wake up at 7 am");
+    console.log("Brush your teeth");
     argument();
-    console.log("take rest for a while");
-    console.log("start study");
-    console.log("study " + data.subjects[0]);
-    console.log("get eady for school");
-    console.log("go to school");
+    console.log("Take rest for a while");
+    console.log("Start study");
+    console.log("Study " + data.subjects[0]);
+    console.log("Get eady for school");
+    console.log("Go to school");
     console.log("Return home from school");
     console.log("Take lunch");
     console.log("Eat " +data.food2[0]+" , " +data.food2[1]+ " and "+data.food2[4]);
     console.log("Take rest for a while");
-    console.log("go to play");
-    console.log("play " +data.sports[2]);
-    console.log("come back home ");
-    console.log("start study and do your homework ");
-    console.log("go to sleep");
+    console.log("Go to play");
+    console.log("Play " +data.sports[2]);
+    console.log("Come back home ");
+    console.log("Start study and do your homework ");
+    console.log("Go to sleep");
 }
  process(breakfast);           
